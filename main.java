287723ede0c8f3080c795d42aeae1b2a46c2da74
nobodyn0se <nobodyn0se@protonmail.ch>
2022-02-25 01:07:19 +0530

    Added initial read code to main

--- main.java
+++ main.java
@@ -1,6 +1,29 @@
 import java.util.*;
-class main{
-    public static void main(String args[]){
+import java.io.*;
 
+import models.*;
+
+class main{
+    static int nContributors;
+    static int nProjects;
+
+    public static void input() {
+        try{
+            FileInputStream fstream=new FileInputStream("./input/input_file.txt");
+            DataInputStream in=new DataInputStream(fstream);
+            BufferedReader br=new BufferedReader(new InputStreamReader(in));
+            String strLine=br.readLine();
+
+            System.out.println(strLine);
+
+            in.close();
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong!");
+        }
+    }
+
+    public static void main(String args[]){
+        input();
     }
 }
