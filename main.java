5efb32a0fff11f1d7045558a1d75fe50a7bae1b3
vancityAyush <eliasakeleven@gmail.com>
2022-02-25 02:25:15 +0530

    contributor done

--- main.java
+++ main.java
@@ -7,7 +7,7 @@ import java.io.*;
 class main{
     static int nContributors;
     static int nProjects;
-    static ArrayList<Contributor> contributorList;
+    static ArrayList<Contributor> contributorList = new ArrayList<>();
 
     public static void input() {
         try{
@@ -21,7 +21,7 @@ class main{
             nContributors = Integer.parseInt(tokens[0]);
             nProjects = Integer.parseInt(tokens[1]);
 
-            for(int i = 0; i < nContributors; ++i) {
+            for(int i = 0; i < nContributors; ++i) {  //CONTRIBUTOR LOOP
                 strLine = br.readLine();
                 tokens = strLine.split(" ");
 
@@ -29,24 +29,17 @@ class main{
                 int nSkills = Integer.parseInt(tokens[1]);
                 //System.out.println(contributorName + " " + nSkills + 1);
                 Contributor contributorObject = new Contributor(contributorName, nSkills);
-                System.out.println(contributorObject.toString());
 
-                for(int j=0; j<nSkills; ++j) {
+                for(int j=0; j<nSkills; ++j) { // SKILL LOOP
                     strLine = br.readLine();
                     tokens = strLine.split(" ");
 
                     String skillName = tokens[0];
-                    System.out.println(skillName);
                     int skillLevel = Integer.parseInt(tokens[1]);
-
                     contributorObject.addSkill(skillName, skillLevel);
-                    System.out.println(skillName);
                 }
-
-
-                //contributorList.add(new Contributor(tokens[0], ));
+                contributorList.add(contributorObject);
             }
-
             in.close();
         }
         catch (Exception e) {
