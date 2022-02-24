e95da7f47284a80893d9cc8402a2b1f9107b0ccb
vancityAyush <eliasakeleven@gmail.com>
2022-02-25 01:03:49 +0530

    model

--- models/Contributor.java
+++ models/Contributor.java
@@ -11,6 +11,24 @@ public class Contributor {
     }
     void addSkill(String name, int level){
         Skill newSkill = new Skill(level, name);
-
+        skills.add(newSkill);
+    }
+    boolean canWork(Role role){
+        for(Skill skill : skills){
+            if(role.name==skill.name){
+                if(role.level>=skill.level)
+                    return true;
+            }
+        }
+        return false;
+    }
+    boolean canWorkWithMentor(Role role){
+        for(Skill skill : skills){
+            if(skill.name==role.name){
+                if(role.level-skill.level==1)
+                    return true;
+            }
+        }
+        return false;
     }
 }
